05095b7be0709140e6cb06648cc6e51840616765
Nate Maxwell <ntmaxwell@yahoo.com>
2026-04-01 16:59:56 -0700

    Add function to attack each other's board
    -Create game boards
    -Input to attack
    -Track hits and missses
    -update life total

--- Battleship.java
+++ Battleship.java
@@ -2,15 +2,64 @@ import java.util.Scanner;
 public class Battleship {
 	public static void main(String[] args) {
 
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Welcome to Battleship!");
 
-		char[][] player1_board = shipPlacement("Player 1");
+		String player1 = "Player 1";
+		String player2 = "Player 2";
+
+		char[][] player1_board = shipPlacement(player1, input);
 		printBattleShip(player1_board);
 		print100();
 
-		char[][] player2_board = shipPlacement("Player 2");
+		char[][] player2_board = shipPlacement(player2, input);
 		printBattleShip(player2_board);
 		print100();
+
+		char[][] player1_targetHistory = createGrid();
+		char[][] player2_targetHistory = createGrid();
+
+		int player1_lifeTotal = 5;
+		int player2_lifeTotal = 5;
+
+		do {
+
+			player2_lifeTotal = chooseTarget(player1_targetHistory, player2_board, player1, player2, input, player2_lifeTotal);
+			if (player2_lifeTotal == 0) {
+				break;
+			}
+			player1_lifeTotal = chooseTarget(player2_targetHistory, player1_board, player2, player1, input, player1_lifeTotal);
+			if (player1_lifeTotal == 0) {
+				break;
+			}
+		} while(true);
+
+		if (player1_lifeTotal == 0) {
+			System.out.println(player2 + " WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+			System.out.println(" ");
+			System.out.println("Final Boards");
+			System.out.println(" ");
+
+			System.out.println("Player 1's Board");
+			printBattleShip(player1_targetHistory);
+
+			System.out.println("Player 2's Board");
+			printBattleShip(player2_targetHistory);
+		}
+		else {
+			System.out.println(player1 + " WINS! YOU SUNK ALL OF YOUR OPPONENT'S SHIPS!");
+			System.out.println(" ");
+			System.out.println("Final Boards");
+			System.out.println(" ");
+
+			System.out.println("Player 1's Board");
+			printBattleShip(player1_targetHistory);
+
+			System.out.println("Player 2's Board");
+			printBattleShip(player2_targetHistory);
+		}
+	
 
 		
 
@@ -52,13 +101,11 @@ public class Battleship {
 		}
 	}
 
-	private static char[][] shipPlacement(String playerName) {
+	private static char[][] shipPlacement(String playerName, Scanner input) {
 
 		char[][] playerBoard = createGrid();
 
 		System.out.println(playerName + ", ENTER YOUR SHIP'S COORDINATES");
-
-		Scanner input = new Scanner(System.in);
 
 		
 		for (int i = 0; i < 5; i++) {
@@ -88,5 +135,50 @@ public class Battleship {
 		}
 
 		return playerBoard;
+	}
+
+	private static int chooseTarget (char[][] historyBoard, char[][] playerBoard, String player1, String player2, Scanner input, int lifeTotal) {
+
+		System.out.println(player1 + ", enter hit row/column");
+
+		boolean validInput = true;
+
+		do {
+			String coordinates = input.nextLine();
+			String[] parts = coordinates.split(" ");
+			int x = Integer.parseInt(parts[0]);
+			int y = Integer.parseInt(parts[1]);
+
+			if (x > 4 || x < 0 || y > 4 || y < 0) {
+				System.out.println("Invalid Coordinates. Choose different coordinates (0-4)");
+				}
+			else if (historyBoard[x][y] != '-') {
+				System.out.println("You already fired on this spot. Choose different coordinates.");
+			}
+			else if (historyBoard[x][y] == '-') {
+				if (playerBoard[x][y] == '-') {
+					System.out.println(player1 + " MISSED!");
+					historyBoard[x][y] = 'O';
+					System.out.println(player1 + "'s Target History Board");
+					printBattleShip(historyBoard);
+					validInput = false;
+				}
+				else if (playerBoard[x][y] == '@') {
+					System.out.println(player1 + " HIT " + player2 + "'S SHIP!");
+					historyBoard[x][y] = 'X';
+					System.out.println(player1 + "'s Target History Board");
+					printBattleShip(historyBoard);
+					lifeTotal--;
+					System.out.println(player2 + "'s LIFE TOTAL: " + lifeTotal);
+					validInput = false;
+				}
+			}
+			else {
+				System.out.println("WE RAN INTO AN UNEXPECTED ERROR");
+			}
+		} while (validInput);
+
+		return lifeTotal;
+
 	}
 }
